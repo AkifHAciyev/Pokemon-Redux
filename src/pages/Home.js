9304fa5c1259afda29ff64609cda4ef4a0051ffc
AkifHaciyev <akif6563@mail.ru>
2023-01-17 04:49:46 +0400

    bla

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPokemons } from '../store/pokemonSlice';
 
 const Home = () => {
-	return <div>Home</div>;
+	const allPokemons = useSelector((state) => state.pokemon.allPokemons);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(getPokemons());
+	}, [dispatch]);
+
+	const [pokeDex, setPokeDex] = useState();
+
+	console.log(pokeDex);
+
+	return (
+		<div className="wrapper">
+			<div className="left">
+				{allPokemons?.map((pokemon) => (
+					<div className="card" onClick={() => setPokeDex(pokemon)}>
+						<h2>{pokemon.url.split('/')[pokemon.url.split('/').length - 2]}</h2>
+						<img
+							src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${
+								pokemon.url.split('/')[pokemon.url.split('/').length - 2]
+							}.png`}
+							alt=""
+						/>
+						<h2>{pokemon.name}</h2>
+					</div>
+				))}
+
+				<div className="btn-group">
+					<button>Previous</button>
+					<button>Next</button>
+				</div>
+			</div>
+			<div className="right">
+				<div className="pokemonInfo">
+					<h1>{pokeDex?.name || 'bulbasaur'}</h1>
+					<img
+						src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${
+							pokeDex?.url.split('/')[pokeDex.url.split('/').length - 2] || 1
+						}.png`}
+						alt=""
+					/>
+
+					<div className="abilities">
+						<p>torrent</p>
+						<p>rain-dish</p>
+					</div>
+					<div className="stats">
+						<p>hp: 45</p>
+						<p>attack: 45</p>
+						<p>defense: 45</p>
+						<p>special-attack: 45</p>
+						<p>special-defense: 45</p>
+						<p>speed: 45</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
---
+++ src/store/pokemonSlice.js
@@ -0,0 +1,41 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const getPokemons = createAsyncThunk('pokemons/getPokemons', async () => {
+	const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=100`);
+	return res.data;
+});
+
+export const pokemonsSLice = createSlice({
+	name: 'pokemons',
+	initialState: {
+		allPokemons: [],
+		status: null,
+		error: null,
+		page: 0,
+		currentPage: 0,
+	},
+	reducers: {
+		setPokemons: (state, action) => {
+			state.allPokemons = action.payload;
+		},
+	},
+	extraReducers: {
+		[getPokemons.pending]: (state) => {
+			state.status = 'pending';
+		},
+		[getPokemons.fulfilled]: (state, action) => {
+			state.allPokemons = [...action.payload.results];
+			state.pages = action.payload.count;
+			state.status = 'fulfilled';
+		},
+		[getPokemons.rejected]: (state, action) => {
+			state.status = 'rejected';
+			state.error = action.error.message;
+		},
+	},
+});
+
+export const { setPokemons } = pokemonsSLice.actions;
+
+export default pokemonsSLice.reducer;
